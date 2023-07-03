46d3f9e76c190bc1931cb44b8af22cf8ba509341
Phyllis Atieno <atienophyllis032@gmail.com>
2023-07-03 16:29:08 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let data = {
             name: "Oyin Dawodu",
             date: "- Apr, 18th 2023",
             title: "Contributing articles to Chimoney Community",
-            article: "Are you passionate about sharing your expertise and making a valuable contribution to an active online community?</br> The Chimoney Community Blog is a dynamic platform that welcomes technical articles on a variety of topics such as software development, ..."
+            article: "."
             tag1:  "Chimoney news",
             tag2: "project utility",
             time: "4 mins read"
@@ -18,7 +18,7 @@ let data = {
             name: "Oyin Dawodu",
             date: "- Apr, 18th 2023",
             title: "Getting Started with Chimoney's API",
-            article: "Chimoney is a payment platform that enables event organizers, businesses, and individuals to securely send money across the world, quickly and efficiently. </br>The platform is designed to make it easy for people to connect and send money to their loved ones, friends, and clients.",
+            article: ".",
             tag1:  "chimoney news",
             tag2: "project utility",
             time: "3 mins read"
