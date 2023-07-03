5eb65be87082f1df8f0e14be924a9adfac1639c8
Phyllis Atieno <atienophyllis032@gmail.com>
2023-07-03 15:52:15 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,13 @@ let data = {
     "data":[
         {
             id: 1,
-            name: "Poppy M",
-            date: "- Feb, 16th 2023",
-            title: "Chimoney Chooses WireFX to Power Payments and Account",
-            article: "Cryptocurrencies like Bitcoin, XRP, and Eth have become increasingly popular over the past few years. They're also becoming an important part of our...",
-            tag1:  "chimoney news",
+            name: "Oyin Dawodu",
+            date: "- Apr, 18th 2023",
+            title: "Contributing articles to Chimoney Community",
+            article: "Are you passionate about sharing your expertise and making a valuable contribution to an active online community?</br> The Chimoney Community Blog is a dynamic platform that welcomes technical articles on a variety of topics such as software development, ..."
+            tag1:  "Chimoney news",
             tag2: "project utility",
-            time: "3 mins read"
+            time: "4 mins read"
         },
         {
             id: 2,
